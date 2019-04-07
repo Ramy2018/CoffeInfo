8f0ddf8d6932b63cc25e1732511e5f6fc9e3e95e
ramy <ramy.niranjan@gmail.com>
2019-04-07 20:56:16 +0200

    Add project

---
+++ WiredBrainCaffeInfo/WiredBrainCaffeInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCaffeInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wired coffe shop - info ");
+        }
+    }
+}
